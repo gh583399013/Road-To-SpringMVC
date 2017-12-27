3520f23bfa1318880227c270ebac44296924f787
gh583399013 <583399013@qq.com>
2017-12-27 15:39:59 +0800

    试试 提交时的包整理

--- spring-JunitTestController/src/main/java/junit_test/test/JunitTest.java
+++ spring-JunitTestController/src/main/java/junit_test/test/JunitTest.java
@@ -1,28 +1,18 @@
 package junit_test.test;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;  
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;  
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;  
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;  
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-
-import javax.servlet.http.HttpServletResponse;
-
-import org.junit.Before;  
-import org.junit.Test;  
-import org.junit.runner.RunWith;  
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.ContextConfiguration;  
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;  
-import org.springframework.test.context.transaction.TransactionConfiguration;  
-import org.springframework.test.context.web.WebAppConfiguration;  
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.transaction.annotation.Transactional;  
-import org.springframework.web.context.WebApplicationContext;  
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
   
 /** 
  * @author zhzh 
